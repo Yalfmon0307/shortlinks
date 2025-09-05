8f1e145843a656897afe17f7b4115492989f9331
Yalfmon0307 <montesyalfred0@gmail.com>
2025-09-04 20:37:55 -0600

    ruta

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/', router);
+app.use('/api', router);
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
